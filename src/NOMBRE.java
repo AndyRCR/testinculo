31a32ef2ba9e739a6ca45a043ddd7e3bf6304f60
= <=>
2020-12-09 20:47:28 -0500

    Mabmab soplanucas

---
+++ src/NOMBRE.java
@@ -0,0 +1,17 @@
+
+public class NOMBRE {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        String nombre = "Miguel";
+        System.out.println("Miguel");
+        
+        String andy = "El puto amo";
+        String miguel = "hola ";
+        //String andy = "Puto";
+        System.out.println(miguel + andy);
+        
+        String miguel2 = "El mas chupapinga";
+    }
+}
