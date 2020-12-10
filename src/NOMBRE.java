fa457264efc6ab180dbefe54659a7cd01ff7423b
MabMab^^ <miguelberrioh@hotmail.com>
2020-12-09 21:16:44 -0500

    Miguel

--- src/NOMBRE.java
+++ src/NOMBRE.java
@@ -6,14 +6,16 @@ public class NOMBRE {
         int num = 0;
         String nombre = "Miguel";
         System.out.println("Miguel");
-        
+
         String andy = "El puto amo";
         String miguel = "hola ";
         //String andy = "Puto";
         System.out.println(miguel + andy);
-        
+
         String miguel2 = "El mas chupapinga";
-        
+
         String g = "Giusseppe";
+
+        String migueltest = "Aqui estuvo MabMab^^";
     }
 }
